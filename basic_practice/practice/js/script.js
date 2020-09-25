807e1e870f44d2660b035c3e3ba29f65cc1c4a42
shotaro1395 <68629378+shotaro1395@users.noreply.github.com>
2020-09-25 16:29:11 +0900

    a11

--- basic_practice/practice/js/script.js
+++ basic_practice/practice/js/script.js
@@ -1,9 +1,11 @@
 
-$('.one-time').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
+$(function(){
+
+  $("#slide").slick({dots:true});
+
+  // $(".btn").on("click",()=>{
+  //   $(".slide").slideToggle();
+  // })    
+
+});
           
